93beabcf7fd658d2cfc8837374f01b4375208d16
alex <levy_a@Vanessa-mbp-2.local>
2016-11-04 14:50:09 +0100

    css page de connexion

--- www/js/controller/LoginCtrl.js
+++ www/js/controller/LoginCtrl.js
@@ -1,3 +1,14 @@
-app.controller('LoginCtrl', function(){
+app.controller('LoginCtrl', ['$scope','$http' , '$location', function($scope, $http, $location) {
 
-})
+$scope.symbols = ['square', 'circle', 'tri', 'cross'];
+$scope.symbolsPassword = [];
+
+$scope.addSymbol = function (symbol) {
+		$scope.symbolsPassword.push(symbol);
+};
+
+$scope.removeSymbol = function (index) {
+		$scope.symbolsPassword.splice(index, 1);
+};
+
+}]);
