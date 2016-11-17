208b1b96fa964a47bb0a16ea6a7689fd913bf345
unknown <vasax.levy@gmail.com>
2016-11-17 17:19:37 +0100

    Modif pour communication + insert des scores

--- www/js/controller/GamePlayCtrl.js
+++ www/js/controller/GamePlayCtrl.js
@@ -1,7 +1,7 @@
 app.controller('GamePlayCtrl', ['$scope','$http' , '$location', '$rootScope', '$state', '$sce',  function($scope, $http, $location, $rootScope, $state, $sce) {
 
 if ($rootScope.co != undefined && $rootScope.co == 1) {
-	$scope.game_url = "http://46.105.123.11/" +  $state.params.game  + "/index.html"
+	$scope.game_url = "http://46.105.123.11/" +  $state.params.game  + "/index.html?user_id=" + $rootScope.user.user_id + "&game_id=" + 1 
 } else {
 	$state.go("login");
 	return;
@@ -10,4 +10,5 @@ if ($rootScope.co != undefined && $rootScope.co == 1) {
 $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
 }
+
 }]);
